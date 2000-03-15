193f95ae08447617f597a387838a79d4c2bda0a0
sarnold <sarnold@83d11ac0-8827-0410-9637-ed2d7c09aec0>
2000-03-15 19:20:24 +0000

    complete body (finally)
    git-svn-id: https://megatron.arnolds.bogus/svn/home/Ada/CS-152/list_manager/trunk@29 83d11ac0-8827-0410-9637-ed2d7c09aec0

--- list_manager.adb
+++ list_manager.adb
@@ -1,10 +1,10 @@
--------------------------------------------------------------
+----------------------------------------------------------------------------
 -- Allan Hancock College
 -- CS152, Spring 2000
 -- Assignment List Package (Array Implementation)
 -- Stephen L Arnold
 -- generic package body List_Manager
--------------------------------------------------------------
+----------------------------------------------------------------------------
 -- Description: This package provides services to save items in a list.
 -- The items to be saved in the list are defined via the generic type
 -- parameter Element_Type.  The maximum size of the list is defined by
@@ -14,21 +14,16 @@
 --
 -- This version of the package body implements List_Type as a record,
 -- with an array to hold the user data.
--------------------------------------------------------------
+----------------------------------------------------------------------------
 
 with Ada.Exceptions;
 package body List_Manager is
-   
-   -- exceptions:
-   --     Overflow             Rasied when list space runs out.
-   --     Cursor_Error     Rasied for invalid cursor operations.
-   --     State_Error       Raised on invalid state change.
-   
-   ----------------------------------------------------------
+      
+   -------------------------------------------------------------------------
    procedure Insert(
 		    Item   : in     Element_Type;
 		    List   : in out List_Type;
-		    Place  : in     Position_Reference := At_End);
+		    Place  : in     Position_Reference := At_End) is
    
    -- Inserts Item into List at the Place specified.  After the insertion, the
    -- cursor is positioned at the newly inserted Item.  If the List is
@@ -38,78 +33,122 @@ package body List_Manager is
    --    Overflow      Item could not be inserted into List.
    --    State_Error   List is in a traversal.
    
-   ----------------------------------------------------------
+   begin -- Insert
+      if List.Count = Max_Size then
+	 Ada.Exceptions.Raise_Exception (Overflow'identity,
+			 "Error using Insert.  List is already full.") ;
+      else
+	 if List.Count = 0 then
+	    List.List_Data'First := Item ;
+	    List.Head := 1 ;
+	 elsif Place = At_End then
+	    List.List_Data(List.Count + 1) := Item ;
+	 elsif Place = Before then
+	    List.List_Data(List.Cursor + 1..List.Tail + 1) := List.List_Data(List.Cursor..List.Tail) ;
+	    List.List_Data(List.Cursor) := Item ;
+	 elsif Place = After then
+	    List.List_Data(List.Cursor + 2..List.Tail + 2) := List.List_Data(List.Cursor + 1..List.Tail + 1) ;
+	    List.List_Data(List.Cursor + 1) := Item ;
+	 else
+	    List.List_Data(List.Head + 1..List.Tail + 1) := List.List_Data(List.Head..List.Tail) ;
+	    List.List_Data(List.Head) := Item ;
+	 end if ;
+	 List.Count := List.Count + 1 ;
+	 List.Tail := List.Count ;
+      end if ;
+   end Insert ;
+	    
+   -------------------------------------------------------------------------
    procedure Replace(
 		     New_Item   : in     Element_Type;
 		     Old_Item   : in out Element_Type;
-		     List       : in out List_Type);
+		     List       : in out List_Type) is
    
    -- Replaces an Item in The List.  The Old_Item is returned and replaced with
    -- the New_Item. The item to be replaced is the one at the current cursor
    -- position.
    
-   -- Exceptions:
-   --    Cursor_Error     List is empty.
+   begin -- Replace
+      
+      if Empty(List) then
+	 Ada.Exceptions.Raise_Exception (Coursor_Error'identity,
+			 "Error using Replace.  List is empty.") ;
+      else
+	 Old_Item := List.List_Data(List.Cursor) ;
+	 List.List_Data(List.Cursor) := New_Item ;
+      end if ;
+   end Replace;
    
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    procedure Remove(
 		    Item   : in out Element_Type;
-		    List   : in out List_Type);
+		    List   : in out List_Type) is
    
    -- Removes the Item in List at the current cursor position and returns
    -- Item.  The cursor is placed at the item immediately preceding the item
    -- being removed.  If the item to be removed was at the head of the list,
    -- the cursor is placed at the new head of the list.
    
-   -- Exceptions:
-   --    Cursor_Error     List is empty.
-   --    State_Error      List is in a traversal.
+   begin -- Remove
+       if Empty(List) then
+	 Ada.Exceptions.Raise_Exception (Coursor_Error'identity,
+			 "Error using Remove.  List is empty.") ;
+       elsif List.Traversing then
+	 Ada.Exceptions.Raise_Exception (State_Error'identity,
+			 "Error using Remove.  List is in a traversal.") ;
+       else
+	  if List.Cursor = List.Tail then
+	     Item := List.List_Data(List.Cursor) ;
+	  else
+	     Item := List.List_Data(List.Cursor) ;
+	     List.List_Data(List.Cursor..List.Tail) := List.List_Data(List.Cursor + 1..List.Tail + 1) ;
+	  end if ;
+       	  List.Count := List.Count - 1 ;
+	  List.Tail := List.Count ;
+       end if ;
+   end Remove;
    
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    procedure Clear(List   : in out List_Type) is
    
-   -- Removes all items from the list.  If the list is empty, the procedure
-   -- does nothing.
+   -- Resets list attributes to zero (removes all items from the list). 
+   -- If the list is empty, the procedure does nothing.
    
    begin  -- Clear
-      if Empty(List) then
-	 return ;
-      elsif List.Traversing then
-	 Ada.Exceptions.Raise_Exception (State_Error'identity,
-               "Error using Clear.  List is in a traversal.") ;
-      else
-	 List.Count := 0 ;
-	 List.Head := 0 ;
-	 List.Tail := 0 ;
-	 List.Cursor := 0 ;
-	 List.Traversing := False ;
+      if not Empty(List) then
+	 if List.Traversing then
+	    Ada.Exceptions.Raise_Exception (State_Error'identity,
+			    "Error using Clear.  List is in a traversal.") ;
+	 else
+	    List.Count := 0 ;
+	    List.Head := 0 ;
+	    List.Tail := 0 ;
+	    List.Cursor := 0 ;
+	    List.Traversing := False ;
+	 end if ;
       end if ;
    end Clear ;
    
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    function Empty(List   : in List_Type) return Boolean is
    
    -- Returns true if the List is empty and false otherwise.
    
-   begin
+   begin -- Empty
       return List.Count = 0 ;
    end Empty ;
    
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    function Count(List   : in List_Type) return Natural is
    
    -- Returns the number of items in the List.  If List is empty, zero is
    -- returned.
    
-   begin  -- Clear
-      if Empty(List) then
-	 return 0 ;
-      else
-	 return List.Count ;
-      end if ;
+   begin  -- Count
+      return List.Count ;
    end Count ;
    
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    procedure Move(
 		  List  : in out List_Type;
 		  Place : in     EndPoint) is
@@ -118,7 +157,7 @@ package body List_Manager is
    -- jump to a given endpoint of the List.
    
    begin -- Move
-      if List.Count = 0 then
+      if Empty(List) then
 	 Ada.Exceptions.Raise_Exception (Coursor_Error'identity,
                "Error using Move.  List is empty.") ;
       elsif List.Traversing then
@@ -131,7 +170,7 @@ package body List_Manager is
       end if ;
    end Move ;
    
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    procedure Move(
 		  List   : in out List_Type;
 		  Course : in     Direction) is
@@ -161,7 +200,7 @@ package body List_Manager is
       end if ;
    end Move ;
    
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    function At_Start(List   : in List_Type) return Boolean is
    
    -- Returns true if the cursor for List is positioned at the start of List.
@@ -175,7 +214,7 @@ package body List_Manager is
       end if ;
    end At_Start ;
    
-  ----------------------------------------------------------
+  --------------------------------------------------------------------------
    function At_End(List   : in List_Type) return Boolean is
    
    -- Returns true if the cursor for List is positioned at the end of List.
@@ -189,7 +228,7 @@ package body List_Manager is
       end if ;
    end At_End ;
    
-  ----------------------------------------------------------
+  --------------------------------------------------------------------------
    function Current_Item(List   : in List_Type) return Element_Type is
    
    -- Returns the item in List at the current cursor position.
@@ -203,28 +242,40 @@ package body List_Manager is
       end if ;
    end Current_Item ;
 
-   ----------------------------------------------------------
+   -------------------------------------------------------------------------
    function "="(Left, Right  : in List_Type) return Boolean is
    
    -- Returns true if both lists have the same number of elements in the
    -- same order, and items at the same relative positions are equal.
    -- Returns false otherwise.
+      
+      Loop_Count : Natural := 0 ;
    
    begin -- "="
       if Left.Count /= Right.Count then
 	 return False ;
-      elsif Empty(Left) and Empty(Right) then
+      elsif Empty(Left) and Empty(Right) then  -- empty lists are equal.
 	 return True ;
+      else 
+	 for I in 1..List.Count loop
+	    exit when Left.Cursor /= Right.Cursor ;
+	    Loop_Count := Positive'Succ(Loop_Count) ;
+	 end loop ;
+	 if Loop_Count /= List.Count then  -- early exit => lists are not equal.
+	   return False ;
+	 else
+	    return True ;
+	 end if ;
       end if ;
    end "=" ;
    
-   ----------------------------------------------------------
-   procedure Traverse(List : in out List_Type; Course : in Direction);
+   -------------------------------------------------------------------------
+   procedure Traverse(List : in out List_Type; Course : in Direction) is
    
    -- Passive list iterator.  Iteration begins at the cursor position and
-   -- advances in the direction indicated by Course until an endpoint of
-   -- List is reached.  Upon return, the cursor is positioned at an endpoint
-   -- of the list, unless the traversal was terminated early.  Early termination
+   -- advances in the direction indicated by Course until an endpoint of List
+   -- is reached.  Upon return, the cursor is positioned at an endpoint of
+   -- the list, unless the traversal was terminated early.  Early termination
    -- of the traversal can be accomplished by setting the Continue parameter
    -- of the generic formal procedure to false.  The traversal will cease, and
    -- the cursor is left positioned at whatever item was last processed.
@@ -241,6 +292,29 @@ package body List_Manager is
    --     State_Error     Attempt to change List state in procedure Process.
    --     Any exceptions raised within procedure Process are propagated.
    
-
+      Continue : Boolean := True ;
+   begin -- Traverse
+      if not Empty(List) then
+	  if List.Traversing then
+	    Ada.Exceptions.Raise_Exception (State_Error'identity,
+			    "Error using Clear.  List is in a traversal.") ;
+	  else
+	     List.Traversing := True ;
+	     loop
+		Process(List.List_Data(List.Cursor), Continue) ;
+			
+		exit when((Course = Forward) and (List.Cursor = List.Tail)) or
+		((Course = Backward) and (List.Cursor = List.Head)) or not Continue ;
+			
+		if Course = Forward then
+		   List.Cursor := Positive'Succ(List.Cursor);
+		else
+		   List.Cursor := Positive'Pred(List.Cursor);
+		end if ;
+	     end loop ;
+	     List.Traversing := False ;
+	  end if ;
+      end if ;
+   end Traverse ;
 end List_Manager;
 
